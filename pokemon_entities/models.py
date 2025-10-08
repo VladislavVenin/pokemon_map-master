ecdc25a72e8235c6318e31c9b4d0f4cd93a3f20d
VladislavVenin <d4spda@yandex.ru>
2025-10-09 02:31:51 +1100

    add specs to PokemonEntity model

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -18,3 +18,11 @@ class PokemonEntity(models.Model):
                                        default=datetime.datetime.now)
     Disappeared_at = models.DateTimeField(verbose_name="Исчезнет в:",
                                           default=datetime.datetime.now)
+    Level = models.IntegerField(verbose_name="Уровень", default=0, null=True, blank=True)
+    Health = models.IntegerField(verbose_name="Здоровье", default=0, null=True, blank=True)
+    Strength = models.IntegerField(verbose_name="Сила", default=0, null=True, blank=True)
+    Defence = models.IntegerField(verbose_name="Защита", default=0, null=True, blank=True)
+    Stamina = models.IntegerField(verbose_name="Выносливость", default=0, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.Pokemon.title_ru} {self.Level}"
